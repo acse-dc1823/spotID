had the wrong normalization. Added RELU also.

leopard_id/model/TripletNetwork.py
@@ -59,8 +59,10 @@ class CustomResNet(nn.Module):
 
 
 class TripletNetwork(nn.Module):
-    def __init__(self, backbone_model="resnet50", num_dims=256, input_channels=3):
+    def __init__(self, backbone_model="resnet50", num_dims=256, input_channels=3, s=1.0):
         super(TripletNetwork, self).__init__()
+        self.s = s
+        
         if input_channels == 3:
             # Load the pre-trained model directly if there are 3 input channels
             self.final_backbone = timm.create_model(backbone_model, pretrained=True, features_only=False)
@@ -78,18 +80,23 @@ class TripletNetwork(nn.Module):
             raise NotImplementedError("Backbone model must end with a recognizable classifier or fc layer.")
 
         # Define a new embedding layer
+
+        self.activation = nn.ReLU()
         self.embedding_layer = nn.Linear(final_in_features, num_dims)
 
         # Add normalization layer
-        self.normalization = nn.LayerNorm(num_dims)
+        self.normalization = Normalize()
 
     def forward(self, x):
         # Forward pass through the backbone model
         features = self.final_backbone(x)
 
+        features = self.activation(features)
+
         # Pass the output of the backbone's final layer to the embedding layer
         embeddings = self.embedding_layer(features)
 
         # Normalize the embeddings
-        embeddings = self.normalization(embeddings)
-        return embeddings
+        embeddings_normalized = self.normalization(embeddings)
+        embeddings_scaled = self.s * embeddings_normalized
+        return embeddings_scaled